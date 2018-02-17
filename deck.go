6dbabe88a48478aebdab4294d2fe971194007feb
Qasim Laghari <go.jets.laghari@gmail.com>
2018-02-17 13:41:34 -0600

    Return a new deck

--- deck.go
+++ deck.go
@@ -16,11 +16,12 @@ func newDeck() deck {
 	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
 	cardValues := []string{"Ace", "Two", "Three", "Four"}
 
-	for i, suit := range cardSuits {
-		for j, value := range cardValues {
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
 			cards = append(cards, suit+" of "+value)
 		}
 	}
+	return cards
 }
 
 func (d deck) print() {
