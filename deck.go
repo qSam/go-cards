95a3baf971cacf57cdb3d3550b57b61d9bc52e08
Qasim Laghari <go.jets.laghari@gmail.com>
2018-02-22 13:38:33 -0600

    Add a toString() func

--- deck.go
+++ deck.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 // Create a new type of 'deck'
@@ -35,5 +36,5 @@ func deal(d deck, handSize int) (deck, deck) {
 }
 
 func (d deck) toString() string {
-
+	return strings.Join([]string(d), ",")
 }
